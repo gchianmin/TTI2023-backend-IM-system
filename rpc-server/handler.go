17205fe11b3effb0d0b669a464c78d3bcb69be5b
gchianmin <min3568@hotmail.com>
2023-06-13 16:59:18 +0800

    complete assignment

--- rpc-server/handler.go
+++ rpc-server/handler.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"context"
-	"math/rand"
-
+	"strings"
+	"fmt"
+	"time"
 	"github.com/TikTokTechImmersion/assignment_demo_2023/rpc-server/kitex_gen/rpc"
 )
 
@@ -11,21 +12,92 @@ import (
 type IMServiceImpl struct{}
 
 func (s *IMServiceImpl) Send(ctx context.Context, req *rpc.SendRequest) (*rpc.SendResponse, error) {
+	timestamp := time.Now().Unix()
+
+    message := &Message{
+       Message:   req.Message.GetText(),
+       Sender:    req.Message.GetSender(),
+       Timestamp: timestamp,
+    }
+
+	roomID, err := getRoomID(req.Message.GetChat())
+	if err != nil {
+		return nil, err
+	 }
+
+    err = redisClient.SaveMessage(ctx, roomID, message)
+    if err != nil {
+       return nil, err
+    }
+
 	resp := rpc.NewSendResponse()
-	resp.Code, resp.Msg = areYouLucky()
+	resp.Code, resp.Msg = 0, "success"
 	return resp, nil
 }
 
 func (s *IMServiceImpl) Pull(ctx context.Context, req *rpc.PullRequest) (*rpc.PullResponse, error) {
+	roomID, err := getRoomID(req.GetChat())
+	if err != nil {
+		return nil, err
+	 }
+	
+	limit := int64(req.GetLimit())
+
+	if limit == 0 {
+		limit = 10 
+	}
+	start := req.GetCursor()
+	end := start + limit // did not minus 1 on purpose for hasMore check later on
+
+	messages, err := redisClient.GetMessagesByRoomID(ctx, roomID, start, end, req.GetReverse())
+    if err != nil {
+       return nil, err
+    }
+	respMessages := make([]*rpc.Message, 0)
+    var counter int32 = 0
+    var nextCursor int64 = 0
+    hasMore := false
+    for _, msg := range messages {
+       if counter+1 > req.GetLimit() {
+          // having extra value here means it has more data
+          hasMore = true
+          nextCursor = end
+          break // do not return the last message
+       }
+       temp := &rpc.Message{
+          Chat:     req.GetChat(),
+          Text:     msg.Message,
+          Sender:   msg.Sender,
+          SendTime: msg.Timestamp,
+       }
+       respMessages = append(respMessages, temp)
+       counter += 1
+    }
 	resp := rpc.NewPullResponse()
-	resp.Code, resp.Msg = areYouLucky()
+	resp.Messages = respMessages
+    resp.Code = 0
+    resp.Msg = "success"
+    resp.HasMore = &hasMore
+    resp.NextCursor = &nextCursor
 	return resp, nil
 }
 
-func areYouLucky() (int32, string) {
-	if rand.Int31n(2) == 1 {
-		return 0, "success"
-	} else {
-		return 500, "oops"
-	}
+// sort the sender in asc order to standardize a1:a2 and a2:a1
+func getRoomID(chat string) (string, error) {
+	var roomID string
+	lowercase := strings.ToLower(chat)
+	senders := strings.Split(lowercase, ":")
+
+    if len(senders) != 2 {
+        err := fmt.Errorf("invalid Chat ID '%s', should be in the format of user1:user2", chat)
+		return "", err
+    }
+
+    sender1, sender2 := senders[0], senders[1]
+    roomID = fmt.Sprintf("%s:%s", sender1, sender2)
+    if strings.Compare(sender1, sender2) > 0 {
+        roomID = fmt.Sprintf("%s:%s", sender2, sender1)
+    }
+
+    return roomID, nil
 }
--- rpc-server/main.go
+++ rpc-server/main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"log"
-
+	"context"
 	rpc "github.com/TikTokTechImmersion/assignment_demo_2023/rpc-server/kitex_gen/rpc/imservice"
 	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
 	etcd "github.com/kitex-contrib/registry-etcd"
 )
 
+var (
+    redisClient = &RedisClient{}
+)
 func main() {
+	ctx := context.Background()
+
+	// init redis client 
+	err := redisClient.InitClient(ctx, "redis:6379", "")
+
+    if err != nil {
+       log.Fatal(err)
+    }
+
 	r, err := etcd.NewEtcdRegistry([]string{"etcd:2379"}) // r should not be reused.
 	if err != nil {
 		log.Fatal(err)
